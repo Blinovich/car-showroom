d90a80478907cee179d5340601982d25e16b20cd
egora <egora@DESKTOP-EFUPS64>
2023-08-18 00:22:28 +0300

    Добавьте файлы проекта.

---
+++ car showroom/Cars.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Admin.h"
+class Cars : private Admin
+{
+	
+};
+
---
+++ car showroom/Menu.cpp
@@ -0,0 +1,24 @@
+#include "Menu.h"
+#include <iostream>
+
+int Menu::showMenu()
+{
+	std::cout << "Hello!\n\n\tMenu: ";
+	std::cout << "1. Show cars\n2. Sing up\n3. Sign in\n4. Exit";
+	int choice = 0;
+
+	switch (choice) {
+	case 1:
+
+		break;
+	case 2:
+
+		break;
+	case 3:
+
+		break;
+	case 4:
+		return 0;
+	}
+	return;
+}
